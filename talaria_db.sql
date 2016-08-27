617b89e1980118fbc104c4b91023f034e8dde972
Yolo Jolo <jolosimeon@users.noreply.github.com>
2016-08-28 04:45:24 +0800

    updated sql

--- talaria_db.sql
+++ talaria_db.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `talaria_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `talaria_db`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: localhost    Database: talaria_db
 -- ------------------------------------------------------
--- Server version	5.6.22-log
+-- Server version	5.6.20
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,7 +32,7 @@ CREATE TABLE `product` (
   `prod_price` decimal(10,2) NOT NULL DEFAULT '0.00',
   `adder_username` varchar(45) NOT NULL,
   PRIMARY KEY (`prod_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=557 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +41,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (100,1,'Shoes 1','this is a shoe',30.00,'jolosimeon'),(101,1,'Shoes 2','another shoe',50.00,'jolosimeon'),(102,2,'something','addsf',100.00,'james');
+INSERT INTO `product` VALUES (102,2,'Sapatos ni Jolo','YEEEEEEEE',100.00,'james'),(103,4,'Slipper1','',500.00,'aa'),(104,4,'Tsinelas for Pokemon Go','Water Type daw bulbasaur',500.00,'aa'),(105,3,'Sandals ni Pikachu','sandals po ito',300.00,'c');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,7 +59,7 @@ CREATE TABLE `review` (
   `details` varchar(500) DEFAULT NULL,
   `username` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +68,7 @@ CREATE TABLE `review` (
 
 LOCK TABLES `review` WRITE;
 /*!40000 ALTER TABLE `review` DISABLE KEYS */;
-INSERT INTO `review` VALUES (1,444,5,'ssadsd','sdsasd');
+INSERT INTO `review` VALUES (1,444,5,'ssadsd','sdsasd'),(2,102,3,'Amazeballs shoes, must buy!','g'),(3,102,1,'Joke lang, wag na.','g'),(4,102,5,'AYOS MGA SER','jolosimeon'),(5,102,5,'wow','jolosimeon'),(6,102,5,'luto review','g');
 /*!40000 ALTER TABLE `review` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -85,7 +87,7 @@ CREATE TABLE `tlineitem` (
   `unit_price` decimal(10,2) NOT NULL,
   `line_total` decimal(10,2) NOT NULL,
   PRIMARY KEY (`tline_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,7 +96,7 @@ CREATE TABLE `tlineitem` (
 
 LOCK TABLES `tlineitem` WRITE;
 /*!40000 ALTER TABLE `tlineitem` DISABLE KEYS */;
-INSERT INTO `tlineitem` VALUES (1,120,100,5,30.00,150.00),(2,122,101,8,50.00,400.00),(3,122,100,5,30.00,150.00);
+INSERT INTO `tlineitem` VALUES (1,120,100,5,30.00,150.00),(2,122,101,8,50.00,400.00),(3,122,100,5,30.00,150.00),(4,-1,100,24,30.00,720.00),(5,1,100,12,30.00,360.00),(6,1,101,33,50.00,1650.00),(7,123,101,24,50.00,1200.00),(8,123,102,2,100.00,200.00),(9,124,104,500,500.00,250000.00),(10,126,104,10,500.00,5000.00),(11,127,102,50,100.00,5000.00),(12,128,102,12,100.00,1200.00),(13,129,102,4,100.00,400.00);
 /*!40000 ALTER TABLE `tlineitem` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,11 +108,11 @@ DROP TABLE IF EXISTS `transaction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transaction` (
-  `trans_id` int(11) NOT NULL,
+  `trans_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `total` decimal(10,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`trans_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=130 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -119,7 +121,7 @@ CREATE TABLE `transaction` (
 
 LOCK TABLES `transaction` WRITE;
 /*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
-INSERT INTO `transaction` VALUES (120,'james',150.00),(122,'james',550.00);
+INSERT INTO `transaction` VALUES (1,'a',0.00),(120,'james',150.00),(122,'james',550.00),(123,'a',1400.00),(124,'b',250000.00),(125,'c',0.00),(126,'g',5000.00),(127,'g',5000.00),(128,'jolosimeon',1200.00),(129,'g',400.00);
 /*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,7 +145,7 @@ CREATE TABLE `type` (
 
 LOCK TABLES `type` WRITE;
 /*!40000 ALTER TABLE `type` DISABLE KEYS */;
-INSERT INTO `type` VALUES (1,'Boots'),(2,'Sandals'),(3,'Shoes'),(4,'Slippers');
+INSERT INTO `type` VALUES (1,'Boots'),(2,'Shoes'),(3,'Sandals'),(4,'Slippers');
 /*!40000 ALTER TABLE `type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -159,11 +161,11 @@ CREATE TABLE `user` (
   `password` varchar(45) NOT NULL,
   `user_type` int(11) NOT NULL,
   `fname` varchar(45) DEFAULT NULL,
-  `minitial` varchar(1) DEFAULT NULL,
+  `minitial` varchar(4) DEFAULT NULL,
   `lname` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
-  `billing_addr` varchar(100) DEFAULT NULL,
-  `shipping_addr` varchar(100) DEFAULT NULL,
+  `billing_addr` varchar(300) DEFAULT NULL,
+  `shipping_addr` varchar(300) DEFAULT NULL,
   `card_no` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -175,17 +177,9 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('james','abc123',3,'James','K','Sy','james_sy@yahoo.com',NULL,NULL,NULL),('jolosimeon','abc123',1,'Juan Lorenzo','L','Simeon','jolo_simoen@gmail.com','85 blah st.','85 blah st.','1234567891234567');
+INSERT INTO `user` VALUES ('a','b',4,'a','a','a','a@y.com','a a, a, a, a a','a a, a, a, a a','122222222222222222222'),('aa','aa',3,'a','a','a','aa',NULL,NULL,NULL),('acc','acc',3,'acc','acc','cc','acc',NULL,NULL,NULL),('b','b',2,'b','b','b','b@y.c','b b, b, b, b b','b b, b, b, b b','333333333333333333'),('c','c',3,'c','c','c','c@y.c',NULL,NULL,NULL),('d','d',3,'d','d','d','d@g.c',NULL,NULL,NULL),('e','e',4,'a','a','a','e',NULL,NULL,NULL),('g','g',1,'g','g','g','g@y.c','g g, g, g, g g','g g, g, g, g g','123455555555555555'),('james','abc123',3,'James','K','Sy','james_sy@yahoo.com',NULL,NULL,NULL),('jolosimeon','abc123',1,'Juan Lorenzo','L','Simeon','jolo_simoen@gmail.com','85 blah st.','85 blah st.','1234567891234567'),('p','p',3,'Prod','p','p','p',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'talaria_db'
---
-
---
--- Dumping routines for database 'talaria_db'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -196,4 +190,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-08-15 13:06:46
+-- Dump completed on 2016-08-28  4:44:03
